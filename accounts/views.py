36580fc3685c7c6413cf44f2022e3a59d9bc4ba2
Luca DEttorre <ldettorre21@gmail.com>
2018-07-08 22:39:50 +0000

    connected login and register html, connected urls.py in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def get_home(request):
     return render(request,"index.html")
+    
+def login(request):
+    return render(request, 'accounts/login.html')
+    
+def register(request):
+    return render(request, 'accounts/register.html')
